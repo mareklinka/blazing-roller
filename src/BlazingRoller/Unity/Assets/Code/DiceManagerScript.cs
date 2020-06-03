fa84bf5e13c474c5b1aaa060875e501c2a1aa9c6
Marek Linka <marek.linka@outlook.com>
2020-06-03 10:52:23 +0200

    Add support for other dice types

--- src/BlazingRoller/Unity/Assets/Code/DiceManagerScript.cs
+++ src/BlazingRoller/Unity/Assets/Code/DiceManagerScript.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class DiceManagerScript : MonoBehaviour
 {
+    public GameObject prefabD4;
     public GameObject prefabD6;
+    public GameObject prefabD8;
+    public GameObject prefabD10;
+    public GameObject prefabD12;
+    public GameObject prefabD20;
 
     // Start is called before the first frame update
     void Start()
@@ -42,13 +47,14 @@ public class DiceManagerScript : MonoBehaviour
 
         foreach (var c in config.Dice.OrderBy(_ => _.Id))
         {
-            if (c.Sides != 6)
+            var die = CreateDie(c.Sides);
+
+            if (die == null)
             {
                 continue;
             }
 
-            var die = Instantiate(prefabD6, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
-            die.tag = "Dice";
+            SetupDie(die);
 
             dice.Add(die);
         }
@@ -61,5 +67,34 @@ public class DiceManagerScript : MonoBehaviour
             var script = die.GetComponent<DieScript>();
             script.RandomStart(dieSeed);
         }
+    }
+
+    private GameObject CreateDie(int sides)
+    {
+        switch (sides)
+        {
+            case 4:
+                return Instantiate(prefabD4, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
+            case 6:
+                return Instantiate(prefabD6, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
+            case 8:
+                return Instantiate(prefabD8, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
+            case 10:
+                return Instantiate(prefabD10, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
+            case 12:
+                return Instantiate(prefabD12, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
+            case 20:
+                return Instantiate(prefabD20, new Vector3(0, -10, 0), new Quaternion(0, 0, 0, 1));
+            default:
+                Debug.Log($"Invalid die encountered: {sides}");
+                return null;
+        }
+    }
+
+    private void SetupDie(GameObject die)
+    {
+        die.tag = "Dice";
+        die.AddComponent<DieScript>();
+        die.transform.localScale = new Vector3(2, 2, 2);
     }
 }
