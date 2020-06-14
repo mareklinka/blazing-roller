99611c4e74520679ed42f0a154359d4303fc78dc
Marek Linka <marek.linka@outlook.com>
2020-06-14 15:37:14 +0200

    Add custom assembly info Allow release-time versioning

---
+++ src/BlazingRoller/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Blazing Roller")]
+[assembly: AssemblyCompany("http://github.com/mareklinka")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
